1d9e63522241e13bbb39010f2036e1f893ad569e
Marlartate430 <marlartate@gmail.com>
2025-08-13 23:01:12 +0200

    PWM0 funciona

--- pwm/pwm0.c
+++ pwm/pwm0.c
@@ -4,17 +4,19 @@
 int main(void)
 {
 	// Set output PWM pin high ( COMPULSORY )
-	DDRD |= 0x40;
-	DDRD &= 0x4F;
+	DDRD |= 0x60;
+	DDRD &= 0x6F;
+	// 0 1 1 0 _ 0 0 0 0 
 
 	// Set Timer 0 to Fast PWM
-	TCCR0A |= 0x43;
-	// 0 1 0 0 _ 0 0 1 1
+	TCCR0A |= 0xF3;
+	// 1 1 1 1 _ 0 0 1 1
 
-	TCCR0B |= 0x0D;
-	// 0 0 0 0 _ 1 1 0 1
+	TCCR0B |= 0x01;
+	// 0 0 0 0 _ 0 0 0 1
 
-	OCR0A = 0x7F;
+	OCR0A = 0x00;
+	OCR0B = 0x7F;
 
 	// TCCR0A &= 0xFF // We don't know if the reserved bits are set or not and we have to keep the bits of the first digit, so it would result an and of FF, which is the same as not doing anything
 	
